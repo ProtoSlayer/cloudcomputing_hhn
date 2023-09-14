8d4972caeb6003dbb87afb8fd5905285619a4238
Dennis Spohrer <dennis.spohrer@xl2.de>
2023-09-14 15:02:26 +0200

    Create and testing first lambda integration in api gateway

--- code/lib/code-stack.ts
+++ code/lib/code-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as iam from 'aws-cdk-lib/aws-iam'
 import { Construct } from 'constructs';
 
 export class CodeStack extends cdk.Stack {
@@ -10,15 +12,15 @@ export class CodeStack extends cdk.Stack {
     //FIXME: Wollen wir die Ressourcen/Methoden in CDK Coden lassen oder vielleicht Swagger nehmen/OpenAPI?
     const carsApigateway = new cdk.aws_apigateway.RestApi(this, "Cars");
 
-
     //Declare Lambda functions
-    const createCarSchedule = new lambda.Function(this, 'CreateCarSchedule',{
+    const createCarScheduleLambda = new lambda.Function(this, 'CreateCarSchedule',{
       runtime: lambda.Runtime.NODEJS_18_X,
       code: lambda.Code.fromAsset("lambda"),
       handler:"apigw-create-car-schedule.handler",
-    })
+    });
+
+    const getMethod = carsApigateway.root.addResource('cars').addMethod('GET',new LambdaIntegration(createCarScheduleLambda));
 
 
-
-  }
+  };
 }
