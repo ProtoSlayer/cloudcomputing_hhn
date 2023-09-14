a6ba53b4afa7ae425d55ca6b0dfd5101f1ec8b93
Dennis Spohrer <dennis.spohrer@xl2.de>
2023-09-14 11:45:45 +0200

    Create lambda function and api gateway

--- code/lib/code-stack.ts
+++ code/lib/code-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CodeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //FIXME: Welches API Gateway wollen wir verwenden
+    //FIXME: Wollen wir die Ressourcen/Methoden in CDK Coden lassen oder vielleicht Swagger nehmen/OpenAPI?
+    const carsApigateway = new cdk.aws_apigateway.RestApi(this, "Cars");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    //Declare Lambda functions
+    const createCarSchedule = new lambda.Function(this, 'CreateCarSchedule',{
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler:"apigw-create-car-schedule.handler",
+    })
+
+
+
   }
 }
