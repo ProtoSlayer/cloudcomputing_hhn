3842884d55bed183f651e54ebb674a8784f40dbe
Dennis Spohrer <dennis.spohrer@xl2.de>
2023-10-08 20:37:14 +0200

    Redeploy lambdas with python runtime

--- manufacture_cars/lib/manufacture_cars-stack.ts
+++ manufacture_cars/lib/manufacture_cars-stack.ts
@@ -1,13 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import {Stack, StackProps, aws_apigateway as apigateway, aws_lambda as lambda,} from 'aws-cdk-lib';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class ManufactureCarsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+export class ManufactureCarsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-        const carsApigateway = new cdk.aws_apigateway.RestApi(this, "Cars");
+    const carsApigateway = new apigateway.RestApi(this, "Cars");
 
-    const lambdaFn = new NodejsFunction(this, "Hello There")
+//Declare create cars lambda function
+const createCarScheduleLambda = new lambda.Function(this, 'CreateCarSchedule',{
+  runtime: lambda.Runtime.PYTHON_3_11,
+  code: lambda.Code.fromAsset("lambda/create"),
+  handler:"apigw-create-car-schedule.handler",
+});
+
+//Declare read lambda function
+const readCarScheduleLambda = new lambda.Function(this, 'ReadCarSchedule',{
+  runtime: lambda.Runtime.PYTHON_3_11,
+  code: lambda.Code.fromAsset("lambda/read"),
+  handler:"apigw-read-car-schedule.handler",
+});
+
+//Declare update lambda function
+const updateCarScheduleLambda = new lambda.Function(this, 'UpdateCarSchedule',{
+  runtime: lambda.Runtime.PYTHON_3_11,
+  code: lambda.Code.fromAsset("lambda/update"),
+  handler:"apigw-update-car-schedule.handler",
+});
+
+//Declare delete lambda function
+const deleteCarScheduleLambda = new lambda.Function(this, 'DeleteCarSchedule',{
+  runtime: lambda.Runtime.PYTHON_3_11,
+  code: lambda.Code.fromAsset("lambda/delete"),
+  handler:"apigw-delete-car-schedule.handler",
+});
+
+const carsResource = carsApigateway.root.addResource('cars');
+const carResource = carsResource.addResource('{carId}')
+const getCarsMethod = carsResource.addMethod('GET',new LambdaIntegration(createCarScheduleLambda));
+const getCarMethod = carResource.addMethod('GET',new LambdaIntegration(createCarScheduleLambda));
   }
 }
