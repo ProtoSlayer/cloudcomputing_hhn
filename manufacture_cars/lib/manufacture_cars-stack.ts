b26c97a696cdec4f781225011f99c5532d621589
Dennis Spohrer <dennis.spohrer@xl2.de>
2023-10-15 15:50:43 +0200

    Implement create car schedule functionality

--- manufacture_cars/lib/manufacture_cars-stack.ts
+++ manufacture_cars/lib/manufacture_cars-stack.ts
@@ -1,44 +1,63 @@
-import {Stack, StackProps, aws_apigateway as apigateway, aws_lambda as lambda,} from 'aws-cdk-lib';
-import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
-import { Construct } from 'constructs';
-
+import {Stack, StackProps, aws_apigateway as apigateway, aws_lambda as lambda, aws_dynamodb as ddb} from "aws-cdk-lib";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { Construct } from "constructs";
 
 export class ManufactureCarsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const carsApigateway = new apigateway.RestApi(this, "Cars");
+    const carsApigateway = new apigateway.RestApi(this, "Cars",{
+      restApiName: "ManufactureCarsRestAPI"
+    });
 
-//Declare create cars lambda function
-const createCarScheduleLambda = new lambda.Function(this, 'CreateCarSchedule',{
-  runtime: lambda.Runtime.PYTHON_3_11,
-  code: lambda.Code.fromAsset("lambda/create"),
-  handler:"apigw-create-car-schedule.handler",
-});
+    //Declare create cars lambda function
+    const createCarScheduleLambda = new lambda.Function(this, "CreateCarSchedule",{
+      functionName: "CreateCarScheduleLambda",
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset("lambda/create"),
+      handler:"apigw_create_car_schedule.handler",
+    });
 
-//Declare read lambda function
-const readCarScheduleLambda = new lambda.Function(this, 'ReadCarSchedule',{
-  runtime: lambda.Runtime.PYTHON_3_11,
-  code: lambda.Code.fromAsset("lambda/read"),
-  handler:"apigw-read-car-schedule.handler",
-});
+    //Declare read lambda function
+    const readCarScheduleLambda = new lambda.Function(this, "ReadCarSchedule",{
+      functionName: "ReadCarScheduleLambda",
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset("lambda/read"),
+      handler:"apigw_read_car_schedule.handler",
+    });
 
-//Declare update lambda function
-const updateCarScheduleLambda = new lambda.Function(this, 'UpdateCarSchedule',{
-  runtime: lambda.Runtime.PYTHON_3_11,
-  code: lambda.Code.fromAsset("lambda/update"),
-  handler:"apigw-update-car-schedule.handler",
-});
+    //Declare update lambda function
+    const updateCarScheduleLambda = new lambda.Function(this, "UpdateCarSchedule",{
+      functionName: "UpdateCarScheduleLambda",
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset("lambda/update"),
+      handler:"apigw_update_car_schedule.handler",
+    });
 
-//Declare delete lambda function
-const deleteCarScheduleLambda = new lambda.Function(this, 'DeleteCarSchedule',{
-  runtime: lambda.Runtime.PYTHON_3_11,
-  code: lambda.Code.fromAsset("lambda/delete"),
-  handler:"apigw-delete-car-schedule.handler",
-});
+    //Declare delete lambda function
+    const deleteCarScheduleLambda = new lambda.Function(this, "DeleteCarSchedule",{
+      functionName: "DeleteCarScheduleLambda",
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset("lambda/delete"),
+      handler:"apigw_delete_car_schedule.handler",
+    });
 
-const carsResource = carsApigateway.root.addResource('cars');
-const carResource = carsResource.addResource('{carId}')
-const getCarsMethod = carsResource.addMethod('GET',new LambdaIntegration(createCarScheduleLambda));
-const getCarMethod = carResource.addMethod('GET',new LambdaIntegration(createCarScheduleLambda));
-  }
+    const carsResource = carsApigateway.root.addResource("cars");
+    const carResource = carsResource.addResource("{carId}");
+    carsResource.addMethod("GET",new LambdaIntegration(readCarScheduleLambda));
+    carResource.addMethod("GET",new LambdaIntegration(readCarScheduleLambda));
+    carsResource.addMethod("POST", new LambdaIntegration(createCarScheduleLambda));
+    carResource.addMethod("PUT", new LambdaIntegration(updateCarScheduleLambda));
+    carResource.addMethod("DELETE", new LambdaIntegration(deleteCarScheduleLambda));
+
+    const scheduledCarsTable = new ddb.TableV2(this, "ScheduledCarsTable",{
+      tableName: "ScheduledCarsTable",
+      partitionKey: {name: "knr", type: ddb.AttributeType.STRING},
+      sortKey: {name: "timestamp", type: ddb.AttributeType.STRING},
+    });
+
+    scheduledCarsTable.grantWriteData(createCarScheduleLambda);
+    scheduledCarsTable.grantReadData(readCarScheduleLambda);
+    scheduledCarsTable.grantWriteData(updateCarScheduleLambda);
+    scheduledCarsTable.grantWriteData(deleteCarScheduleLambda);
+  };
 }
